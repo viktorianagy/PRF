f6103e1feaf6c82d5b8981266a5d06a6c9da894a
Viktória Nagy <viki.nagy.98@gmail.com>
2021-05-30 20:10:58 +0200

    tranzakcio.java

--- spring/src/main/java/hu/szte/prf/prfprojekt/models/Tranzakcio.java
+++ spring/src/main/java/hu/szte/prf/prfprojekt/models/Tranzakcio.java
@@ -1,5 +1,6 @@
 package hu.szte.prf.prfprojekt.models;
 
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -10,4 +11,47 @@ public class Tranzakcio {
     private int datum;
     private int osszeg;
 
+    //private Termek termek;
+
+    public Tranzakcio() {
+    }
+
+    public Tranzakcio(int termekid, int datum, int osszeg) {
+        this.termekid = termekid;
+        this.datum = datum;
+        this.osszeg = osszeg;
+    }
+
+    public int getTermekId() {
+        return termekid;
+    }
+
+    public void setTermekId(int termekid) {
+        this.termekid = termekid;
+    }
+
+    public int getDatum() {
+        return datum;
+    }
+
+    public void setDatum(int datum) {
+        this.datum = datum;
+    }
+
+    public int getOsszeg() {
+        return osszeg;
+    }
+
+    public void setOsszeg(int osszeg) {
+        this.osszeg = osszeg;
+    }
+
+    @Override
+    public String toString() {
+        return "Tranzakcio [termekid=" + termekid + ", osszeg = " + osszeg + ", datum =" + datum + "]";
+    }
 }
+
+
+
+
